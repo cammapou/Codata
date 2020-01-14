18fbf218f7edcb214d65e237fbc5ae3a7beef26e
camille <cmapoula@gmail.com>
2020-01-14 13:57:46 +0100

    Initial Commit

---
+++ CoData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoData
+//
+//  Created by Ddales on 14/01/2020.
+//  Copyright © 2020 Ddales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
